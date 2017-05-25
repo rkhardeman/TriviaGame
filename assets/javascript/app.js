bf7a226902f44067baea6cd1610fa6f94cbed815
Ryan Hardeman <Ryan Hardeman>
2017-05-25 17:42:24 -0400

    html and js updates for testing

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//declare score counters
+var correct = 0;
+var incorrect = 0;
+
+//create questions
+var questions = [{
+    question: "What is the registry number of the USS Enterprise?",
+    choices:["NCC-1701", "NCC-1702", "NCC-1703", "NCC-1704"],
+    correctAnswer: 0
+  },
+
+  {
+  	question: "What is Captain Kirks middle name?",
+  	choices:["Liam", "Timothy", "Taylor", "Tiberius"],
+  	correctAnswer: 3
+  }
+
+  ];
+
